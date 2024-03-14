f71085526f6d2a9f01d1959d30ad7875cfebdffd
ruanbrito <rpablobrito@gmail.com>
2024-03-13 23:55:05 -0300

    Projeto criado

---
+++ src/test/java/com/projeto/jpa/spring/projetojpaspring/ProjetojpaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.jpa.spring.projetojpaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojpaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
