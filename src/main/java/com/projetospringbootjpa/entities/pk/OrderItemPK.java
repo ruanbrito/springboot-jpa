35c4b03a6ccf87c863847a81b49fe64943292ce4
ruanbrito <rpablobrito@gmail.com>
2024-03-23 12:15:27 -0300

    OrderItem, many-to-many association with extra attributes pt.01

---
+++ src/main/java/com/projetospringbootjpa/entities/pk/OrderItemPK.java
@@ -0,0 +1,53 @@
+package com.projetospringbootjpa.entities.pk;
+
+import java.util.Objects;
+
+import com.projetospringbootjpa.entities.Order;
+import com.projetospringbootjpa.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPK {
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")//coluna FK na tabela order_item
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")//coluna FK na tabela order_item
+	private Product product;
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+
+}
