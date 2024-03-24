cf9a598a4ccc22904f15227b7e3b63d4c3e64043
ruanbrito <rpablobrito@gmail.com>
2024-03-23 23:29:27 -0300

    User insert

--- src/main/java/com/projetospringbootjpa/services/UserService.java
+++ src/main/java/com/projetospringbootjpa/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> user = userRepository.findById(id);
 		return user.get();
 	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
 }
