05e80d6aebbc32a346aa3f4b5417b647a93e553b
ruanbrito <rpablobrito@gmail.com>
2024-04-13 19:24:06 -0300

    tratamento de exceções do metodo delete de UserService

--- src/main/java/com/projetospringbootjpa/services/UserService.java
+++ src/main/java/com/projetospringbootjpa/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetospringbootjpa.entities.User;
 import com.projetospringbootjpa.repositories.UserRepository;
+import com.projetospringbootjpa.services.exceptions.DatabaseException;
 import com.projetospringbootjpa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,9 +33,15 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
+		try {
 		userRepository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity, obj);
@@ -42,6 +51,6 @@ public class UserService {
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());		
+		entity.setPhone(obj.getPhone());
 	}
 }
