6def8fc1425a5a987457ef4ef7adcac91c5b2f3d
ruanbrito <rpablobrito@gmail.com>
2024-04-13 14:36:03 -0300

    implementação do metodod Delete em User

--- src/main/java/com/projetospringbootjpa/resources/UserResource.java
+++ src/main/java/com/projetospringbootjpa/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,6 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
@@ -37,15 +37,18 @@ public class UserResource {
 
 		return ResponseEntity.ok().body(user);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
-		obj= service.insert(obj);
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/projetospringbootjpa/services/UserService.java
+++ src/main/java/com/projetospringbootjpa/services/UserService.java
@@ -27,4 +27,7 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
 }
